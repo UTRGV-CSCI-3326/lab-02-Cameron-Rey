cf93eb0e6d3c61fc6dab41be92a8a7f5c8799af1
Cameron Borrego <161384565+Cameron-Rey@users.noreply.github.com>
2025-01-30 22:52:48 +0000

    my second lab and commit!

---
+++ src/MixMatch.java
@@ -0,0 +1,34 @@
+public class MixMatch {
+
+    public static void main(String[] args) {
+
+        String myWord = "'Peace'";
+        System.out.println(myWord + " <- String, contains a collection of characters surrounded by double quotes");
+
+        char myLetter = 'C';
+        System.out.println(myLetter + " <- char, stores a single character/letter or ASCII values");
+
+        short myShortNum = 11965;
+        System.out.println(myShortNum + " <- short, stores whole numbers from -32,768 to 32,767");
+
+        int myNum = 7;
+        System.out.println(myNum + " <- int, stores whole numbers from -2,147,483,648 to 2,147,483,647");
+
+        long myLongNum = 75653964;
+        System.out.println(myLongNum + " <- long, stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807");
+
+        float myFloatNum = 7.256f;
+        System.out.println(myFloatNum + " <- float, stores fractional numbers. Sufficient for storing 6 to 7 decimal digits");
+
+        double myDoubleNum = 9.333333;
+        System.out.println(myDoubleNum + " <- double, stores fractional numbers. Sufficient for storing 15 to 16 decimal digits");
+
+        boolean myBool = true;
+        System.out.println(myBool + " <- boolean, stores true or false values");
+
+        //practice 
+
+        System.out.println("Time for some practice! " + " | mynum = " + myNum + " | myDoublenum = " + myDoubleNum + " | myFloatNum = " + myFloatNum + " | myBool = " + myBool + " | myLetter = " + myLetter);
+       
+    }
+}
